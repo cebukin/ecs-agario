27d8a73f715401e83abc4c98aeb63223596214c8
Cebukin <bruno.cebukin@gmail.com>
2018-12-16 16:20:31 -0200

    Scale system now runs parallel jobs

--- Assets/Scripts/ScaleSystem.cs
+++ Assets/Scripts/ScaleSystem.cs
@@ -1,10 +1,13 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 using Unity.Transforms;
+using Unity.Collections;
+using Unity.Jobs;
 
 [UpdateAfter(typeof(CollisionSystem))]
-public class ScaleSystem : ComponentSystem
+public class ScaleSystem : JobComponentSystem
 {
     public struct Data
     {
@@ -14,13 +17,28 @@ public class ScaleSystem : ComponentSystem
         public ComponentDataArray<Player> Player;
     }
 
-    [Inject] Data m_Data;
-    protected override void OnUpdate()
+    [BurstCompile]
+    public struct UpdateScaleJob : IJobParallelFor
     {
-        for (int i = 0; i < m_Data.Length; i++)
+        [ReadOnly] public ComponentDataArray<Size> sizes;
+        public ComponentDataArray<Scale> scales;
+
+        public void Execute(int index)
         {
-            float size = m_Data.Size[i].Value;
-            m_Data.Scale[i] = new Scale { Value = new float3(size, size, size) };
+            float size = sizes[index].Value;
+            scales[index] = new Scale { Value = new float3(size, size, size) };
         }
+    }
+
+    [Inject] Data m_Data;
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var updateScaleJob = new UpdateScaleJob
+        {
+            sizes = m_Data.Size,
+            scales = m_Data.Scale
+        };
+
+        return updateScaleJob.Schedule(m_Data.Length, 64, inputDeps);
     }
 }
